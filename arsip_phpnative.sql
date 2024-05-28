712e44b514ebecf16d4c075bc6482e75a2e28af4
Afriyanto <123781429+afrianT0@users.noreply.github.com>
2024-05-28 07:57:32 +0700

    Update arsip_phpnative.sql

--- arsip_phpnative.sql
+++ arsip_phpnative.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: May 01, 2024 at 05:14 AM
--- Server version: 8.0.30
--- PHP Version: 8.2.10
+-- Host: 127.0.0.1
+-- Generation Time: May 28, 2024 at 02:44 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,136 +24,137 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `archives`
+-- Table structure for table `tb_instansi`
 --
 
-CREATE TABLE `archives` (
-  `id` int NOT NULL,
-  `category_id` int NOT NULL,
-  `user_id` int NOT NULL,
-  `instansi_id` int NOT NULL,
+CREATE TABLE `tb_instansi` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `telepon` varchar(50) NOT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+)
+
+--
+-- Dumping data for table `tb_instansi`
+--
+
+INSERT INTO `tb_instansi` (`id`, `nama`, `alamat`, `telepon`, `created_at`, `updated_at`) VALUES
+(1, 'PT Krakatau Global Trading', 'Jakarta', '08123456789', '2024-05-19 17:04:42', '2024-05-19 17:21:16'),
+(2, 'PT Juhdi Sakti Engineering', 'Serang', '025457951001', '2024-05-20 06:29:28', '2024-05-20 15:31:15');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_kategori`
+--
+
+CREATE TABLE `tb_kategori` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+)
+
+--
+-- Dumping data for table `tb_kategori`
+--
+
+INSERT INTO `tb_kategori` (`id`, `nama`, `created_at`, `updated_at`) VALUES
+(1, 'Keuangan', '2024-05-19 15:00:23', '2024-05-19 15:00:23'),
+(3, 'Perpajakan', '2024-05-20 06:29:02', '2024-05-20 06:29:02');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_surat`
+--
+
+CREATE TABLE `tb_surat` (
+  `id` int(11) NOT NULL,
+  `kategori_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `instansi_id` int(11) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `nomor` varchar(50) NOT NULL,
   `tanggal` date NOT NULL,
   `deskripsi` text NOT NULL,
   `file` varchar(255) DEFAULT NULL,
   `tipe_surat` enum('1','2') NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+  `status` enum('1','2','3') NOT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+)
 
 --
--- Dumping data for table `archives`
+-- Dumping data for table `tb_surat`
 --
 
-INSERT INTO `archives` (`id`, `category_id`, `user_id`, `instansi_id`, `nama`, `nomor`, `tanggal`, `deskripsi`, `file`, `tipe_surat`, `created_at`, `updated_at`) VALUES
-(72, 1, 1, 3, 'Invoice', '0047/TJK-KPP/X/23', '2024-04-01', 'TESTESTTEST', '2024-04-01-6612510e6f19a.pdf', '1', '2024-04-07 07:53:50', '2024-04-07 07:53:50'),
-(73, 9, 1, 2, 'Invoice', '0048/TJK-KPP/X/23', '2024-04-07', 'TESTESTTEST', '2024-04-07-6612512206118.pdf', '2', '2024-04-07 07:54:10', '2024-04-07 07:54:10'),
-(74, 10, 39, 2, 'Invoice', '0040/TJK-KPP/X/23', '2024-04-07', 'TESTESTTEST', '2024-04-07-66129867b95df.pdf', '1', '2024-04-07 12:58:15', '2024-04-07 12:58:15');
+INSERT INTO `tb_surat` (`id`, `kategori_id`, `user_id`, `instansi_id`, `nama`, `nomor`, `tanggal`, `deskripsi`, `file`, `tipe_surat`, `status`, `created_at`, `updated_at`) VALUES
+(2, 1, 1, 1, 'Invoice', '001/TJK-KGT/X/23', '2024-05-20', 'TESTESTTEST', 'surat-masuk/2024-05-20-664ae4243d4aa.pdf', '1', '1', '2024-05-20 05:48:20', '2024-05-22 13:06:57'),
+(3, 3, 1, 2, 'SP2DK', '002/TJK-KGT/X/23', '2024-05-19', 'TESTESTTEST', 'surat-keluar/2024-05-19-664aedea0e097.pdf', '2', '3', '2024-05-20 06:30:02', '2024-05-20 15:02:15'),
+(5, 3, 2, 1, 'SP2DK', '003/TJK-KGT/X/23', '2024-05-20', 'TESTESTTEST', 'surat-masuk/2024-05-20-664b6cb814f3e.pdf', '1', '2', '2024-05-20 15:31:04', '2024-05-22 13:06:41');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `categories`
+-- Table structure for table `tb_users`
 --
 
-CREATE TABLE `categories` (
-  `id` int NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
---
--- Dumping data for table `categories`
---
-
-INSERT INTO `categories` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'Keuangan', '2024-02-15 13:50:40', '2024-02-17 13:00:48'),
-(9, 'Perizinan', '2024-02-23 08:09:16', '2024-02-23 08:09:16'),
-(10, 'Perpajakan', '2024-02-23 17:00:35', '2024-04-04 14:41:14');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `instansi`
---
-
-CREATE TABLE `instansi` (
-  `id` int NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `phone` varchar(50) NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
---
--- Dumping data for table `instansi`
---
-
-INSERT INTO `instansi` (`id`, `name`, `address`, `phone`, `created_at`, `updated_at`) VALUES
-(2, 'PT. Juhdi Sakti Engineering', 'Serang', '025457951001', '2024-04-07 07:03:39', '2024-04-07 07:03:39'),
-(3, 'PT. Krakatau Global Trading', 'Cilegon', '081381029077', '2024-04-07 07:26:50', '2024-04-07 07:28:16');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int NOT NULL,
+CREATE TABLE `tb_users` (
+  `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
+  `nama` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `level` varchar(255) NOT NULL,
-  `picture` varchar(255) DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+  `gambar` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+)
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `tb_users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `level`, `picture`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin@email.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Administrator', '6617d9327e9d3.png', '2024-02-15 06:38:58', '2024-04-11 12:36:02'),
-(39, 'user', 'user@email.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'User', '6617d5644b873.png', '2024-04-06 06:11:35', '2024-04-11 12:19:48'),
-(40, 'user1', 'user2@email.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'User', '6617d88d6ba49.png', '2024-04-07 14:03:24', '2024-04-11 12:33:17');
+INSERT INTO `tb_users` (`id`, `username`, `nama`, `email`, `password`, `level`, `gambar`, `created_at`, `updated_at`) VALUES
+(1, 'admin', 'Afrian', 'admin@email.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Administrator', '6617d9327e9d3.png', '2024-02-15 06:38:58', '2024-05-19 17:31:39'),
+(2, 'user', 'User', 'user@email.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'User', 'default.png', '2024-05-19 16:00:06', '2024-05-19 16:07:27'),
+(4, 'validator', 'Validator', 'validator@email.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Validator', 'default.png', '2024-05-20 15:06:52', '2024-05-20 15:06:52');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `archives`
+-- Indexes for table `tb_instansi`
 --
-ALTER TABLE `archives`
+ALTER TABLE `tb_instansi`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nama` (`nama`);
+
+--
+-- Indexes for table `tb_kategori`
+--
+ALTER TABLE `tb_kategori`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nama` (`nama`);
+
+--
+-- Indexes for table `tb_surat`
+--
+ALTER TABLE `tb_surat`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `nomor` (`nomor`),
-  ADD KEY `FK_archives_categories` (`category_id`),
-  ADD KEY `FK_archives_users` (`user_id`),
-  ADD KEY `FK_archives_instansi` (`instansi_id`);
+  ADD KEY `FK_tb_surat_tb_kategori` (`kategori_id`),
+  ADD KEY `FK_tb_surat_tb_users` (`user_id`),
+  ADD KEY `FK_tb_surat_tb_instansi` (`instansi_id`);
 
 --
--- Indexes for table `categories`
+-- Indexes for table `tb_users`
 --
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`);
-
---
--- Indexes for table `instansi`
---
-ALTER TABLE `instansi`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
+ALTER TABLE `tb_users`
   ADD PRIMARY KEY (`id`) USING BTREE,
   ADD UNIQUE KEY `username` (`username`),
   ADD UNIQUE KEY `email` (`email`);
@@ -163,40 +164,40 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `archives`
+-- AUTO_INCREMENT for table `tb_instansi`
 --
-ALTER TABLE `archives`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
+ALTER TABLE `tb_instansi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `categories`
+-- AUTO_INCREMENT for table `tb_kategori`
 --
-ALTER TABLE `categories`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+ALTER TABLE `tb_kategori`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `instansi`
+-- AUTO_INCREMENT for table `tb_surat`
 --
-ALTER TABLE `instansi`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `tb_surat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT for table `tb_users`
 --
-ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+ALTER TABLE `tb_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `archives`
+-- Constraints for table `tb_surat`
 --
-ALTER TABLE `archives`
-  ADD CONSTRAINT `FK_archives_categories` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
-  ADD CONSTRAINT `FK_archives_instansi` FOREIGN KEY (`instansi_id`) REFERENCES `instansi` (`id`),
-  ADD CONSTRAINT `FK_archives_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+ALTER TABLE `tb_surat`
+  ADD CONSTRAINT `FK_tb_surat_tb_instansi` FOREIGN KEY (`instansi_id`) REFERENCES `tb_instansi` (`id`),
+  ADD CONSTRAINT `FK_tb_surat_tb_kategori` FOREIGN KEY (`kategori_id`) REFERENCES `tb_kategori` (`id`),
+  ADD CONSTRAINT `FK_tb_surat_tb_users` FOREIGN KEY (`user_id`) REFERENCES `tb_users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
