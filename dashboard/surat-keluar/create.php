47ee2f59635604a8711c83d89bd31978db27fa6b
Afriyanto <123781429+afrianT0@users.noreply.github.com>
2024-05-28 09:42:18 +0700

    Update create.php

--- dashboard/surat-keluar/create.php
+++ dashboard/surat-keluar/create.php
@@ -144,6 +144,7 @@ $page       = 'Surat Keluar';
                                                     <?php echo isset($_SESSION['gagal']) ? $_SESSION['gagal'] : ''; ?>
                                                 </div>
                                                 <?php unset($_SESSION['gagal']); ?>
+                                                <span>*Maks 2 MB</span>
                                             </div>
                                         </div>
 
